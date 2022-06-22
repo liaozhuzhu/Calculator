c0106f960996c39ca9da760865b0d48be17bd71d
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-21 21:28:02 -0500

    Progress on Functions

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let topScreen = document.getElementById("top-screen");
 let botScreen = document.getElementById("bot-screen");
 let operand = "";
 let result = "";
+let rerun = false;
+let func = "";
 
 function display(input) {
-    topScreen.innerHTML = botScreen.innerHTML;
+    if (rerun == true) {
+        botScreen.innerHTML = "";
+        rerun = false;
+    }
     // Special Cases
     if (input == "C") {
         topScreen.innerHTML = "";
@@ -14,17 +19,31 @@ function display(input) {
     if (input == "=") {
         calculate();
     }
+    if (input == "ans") {
+        useAnswer();
+    }
     // Number Buttons
     else if (input != "C" && input != "=") {
         botScreen.style.fontSize = "30px";
         botScreen.innerHTML += input;
+        topScreen.innerHTML = botScreen.innerHTML;
     }
 }
 
 function calculate() {
-    result = eval(botScreen.innerHTML);
+    if (botScreen.innerHTML == "" || rerun == true) {
+        return;
+    }
+    result = Math.round(eval(botScreen.innerHTML)*1000)/1000;
     botScreen.innerHTML = result;
-    topScreen += " = " + result;
+    ;
+    topScreen.innerHTML += "=" + result;
+    rerun = true;
+}
+
+function useAnswer() {
+    topScreen.innerHTML += result;
+    botScreen.innerHTML += result;
 }
 
 buttons.forEach(function(buttonClick) {
